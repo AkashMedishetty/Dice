6002ef931b083abe95c2ba5dbccf5ce71e52cce2
AkashMedishetty <m.akash7416@gmail.com>
2025-12-06 10:54:53 +0530

    Changed Vercel to mumbai

--- api/prizes.ts
+++ api/prizes.ts
@@ -7,6 +7,9 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+  // Aggressive caching - prizes don't change often
+  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate=300');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
